5a35a23be2c8e7a3ce24addc858c2fcf09aa42ea
vinayak <vinayakmdaz@gmail.com>
2026-06-20 15:27:22 +0530

    create front and backend

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to React 🚀</h1>
+      <p>Hello Vinayak! This is your first React app.</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ travel-app/server/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
